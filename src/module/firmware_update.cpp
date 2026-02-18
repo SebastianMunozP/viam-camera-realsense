99171bce9dbf5bc12f8dba5b2d89155ef43c8927
Sebastian Munoz <sebastian.munoz@viam.com>
2026-02-18 14:36:49 -0500

    Adding comments to readFirmwareFile

--- src/module/firmware_update.cpp
+++ src/module/firmware_update.cpp
@@ -10,15 +10,26 @@ namespace realsense {
 namespace firmware_update {
 
 std::vector<uint8_t> readFirmwareFile(const std::string &file_path) {
+  // Creating a file stream to read the firmware file,
+  // std::ios::binary | std::ios::ate
+  // std::ios::binary: Opens the file in binary mode, which means that the file
+  // is read as a sequence of bytes without any translation of special
+  // characters. std::ios::ate: Opens the file at the end of the file, which
+  // means that the file pointer is positioned at the end of the file, this is
+  // done to calculate its size easily with only one tellg operation.
   std::ifstream file(file_path, std::ios::binary | std::ios::ate);
   if (!file.is_open()) {
     throw std::runtime_error("Failed to open firmware file: " + file_path);
   }
 
+  // Get the size of the file
   std::streamsize size = file.tellg();
+  // Move the file pointer to the beginning of the file
   file.seekg(0, std::ios::beg);
 
+  // Create a vector of bytes to store the firmware data
   std::vector<uint8_t> firmware_data(size);
+  // Read the firmware data from the file
   if (!file.read(reinterpret_cast<char *>(firmware_data.data()), size)) {
     throw std::runtime_error("Failed to read firmware file: " + file_path);
   }
